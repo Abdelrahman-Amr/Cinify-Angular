eaf5ca0f6d87c8bd24c600d011f4a994b3015d72
Hana-20 <hana_20180683@fci.helwan.edu.eg>
2023-06-22 09:51:32 +0300

    getRequests

--- src/app/client/components/clinic-search/clinic-search.component.ts
+++ src/app/client/components/clinic-search/clinic-search.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,15 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./clinic-search.component.css']
 })
 export class ClinicSearchComponent {
-  selectedSpeciality: string;
-  selectedGovernorate: string;
-  specialties: string[] = [
-    "Skin", "Teeth", "Psyciatry", "Ear, Nose and Throat"
-  ];
-  governorates: string[] = [
-    "Giza", "Dakahlia", "Cairo"
-  ];
-  cities: string[] = [
-    "Mansoura", "6th Of October"
-  ]
+
+  selectedSpeciality: string | null = null;;
+  selectedArea: string | null = null;;
+  selectedCity: string | null = null;;
+  doctorName:string | null = null;;
+  specialties: any[] =[]
+  cities: any[] = []
+  Areas: any[] = [];
+  constructor(private _http:HttpClient){
+    console.log(this.specialties);
+  }
+  ngOnInit(): void {
+
+    this.getAllSpecialties();
+    this.getAllAreas();
+    this.getAllCities();
+  }
+
+  getAllSpecialties():void{
+    this._http.get<any>("http://localhost:9999/api/doctorSpecs/all")
+    .subscribe(
+      {
+        next:response=>{
+          this.specialties = response;
+          console.log(response);
+        },
+        error:error=>{}
+      }
+    );
+  }
+  getAllAreas():void{
+    this._http.get<any>("http://localhost:9999/api/areas/all")
+    .subscribe(
+      {
+        next:response=>{
+          this.Areas = response;
+          console.log(response);
+        },
+        error:error=>{}
+      }
+    );
+  }
+  getAllCities():void{
+    this._http.get<any>("http://localhost:9999/api/cities/all")
+    .subscribe(
+      {
+        next:response=>{
+          this.cities = response;
+          console.log(response);  
+        },
+        error:error=>{}
+      }
+    );
+  }
+  getSearchResult(){
+    
+  }
 }
