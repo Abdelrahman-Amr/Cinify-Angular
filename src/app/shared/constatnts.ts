e1e09c66a81bdc62055ff85cf1607513986d8b69
Hana-20 <hana_20180683@fci.helwan.edu.eg>
2023-06-24 02:47:40 +0300

    solve search bug

--- src/app/shared/constatnts.ts
+++ src/app/shared/constatnts.ts
@@ -55,7 +55,7 @@ export class Constants {
   public static readonly updateAppointmentWithoutRatingURL = this.baseURL + '/appointmentsForClinics/update';
   public static readonly deleteAppointmentWithoutRatingUrl = this.baseURL + '/appointmentsForClinics/delete/';
   public static readonly addAppointmentWithoutRatingURL = this.baseURL + '/appointmentsForClinics/add';
-
+ 
   // public static readonly getAppointmentByDoctorIdURL = this.baseURL + '/appointmentsForClinics/byDoctorId/';
   // public static readonly getAppointmentUpcomingByDoctorIdURL = this.baseURL + '/appointmentsForClinics/byDoctorId/upcoming/';
 
