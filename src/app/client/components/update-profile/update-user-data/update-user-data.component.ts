b6ec3c19ec2608fd5b29aa1457c2576d7fb25ea1
Hana-20 <hana_20180683@fci.helwan.edu.eg>
2023-06-25 15:30:35 +0300

    updateRequest

--- src/app/client/components/update-profile/update-user-data/update-user-data.component.ts
+++ src/app/client/components/update-profile/update-user-data/update-user-data.component.ts
@@ -22,35 +22,20 @@ export class UpdateUserDataComponent implements OnInit {
     
    
   }
-  minAge(minAge: number, errorMessage: string): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const birthDate = new Date(control.value);
-      const currentDate = new Date();
-  
-      const diffYears = currentDate.getFullYear() - birthDate.getFullYear();
-      const diffMonths = currentDate.getMonth() - birthDate.getMonth();
-      const diffDays = currentDate.getDate() - birthDate.getDate();
-  
-      if (diffYears < minAge || (diffYears === minAge && diffMonths < 0) || (diffYears === minAge && diffMonths === 0 && diffDays < 0)) {
-        return { 'minAge': { message: errorMessage } };
-      }
-  
-      return null;
-    };
-  }
+
 
   ngOnInit(): void {
     
     this.profileForm = this.formBuilder.group({
       //@ts-ignore
-      name: [patient.fullName,[ Validators.minLength(3),
+      name: [this.patient.fullName,[ Validators.minLength(3),
         Validators.maxLength(30)]],
       //@ts-ignore
-      Email: [patient.email, [Validators.pattern(Constants.EMAIL)]],
+      Email: [this.patient.email, [Validators.pattern(Constants.EMAIL)]],
       //@ts-ignore
-      phone: [patient.phoneNumber,[Validators.pattern(Constants.DIGITS_ONLY_11)]],
+      phone: [this.patient.phoneNumber,[Validators.pattern(Constants.DIGITS_ONLY_11)]],
       //@ts-ignore
-      birthday: [patient.birthDate,[this.minAge(15, 'You must be at least 15 years old')]],
+      birthday: [this.patient.birthDate,[this.patientService.minAge(15, 'You must be at least 15 years old')]],
     });
 
   }
@@ -60,15 +45,15 @@ export class UpdateUserDataComponent implements OnInit {
       this.patient.fullName=this.profileForm.get('name')?.value;
     }
     if(this.profileForm.get('email')?.value!=null||this.profileForm.get('email')?.value!=''){
-      this.patient.fullName=this.profileForm.get('email')?.value;   
+      this.patient.email=this.profileForm.get('email')?.value;   
      }
 
      if(this.profileForm.get('phone')?.value!=null||this.profileForm.get('phone')?.value!=''){
-      this.patient.fullName=this.profileForm.get('phone')?.value;
+      this.patient.phoneNumber=this.profileForm.get('phone')?.value;
      }
 
      if(this.profileForm.get('birthday')?.value!=null||this.profileForm.get('birthday')?.value!=''){
-      this.patient.fullName=this.profileForm.get('birthday')?.value;
+      this.patient.birthDate=this.profileForm.get('birthday')?.value;
      }
     
 
--- src/app/shared/services/patient.service.ts
+++ src/app/shared/services/patient.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { PatientModel } from '../model/patient-model';
 import { HttpClient } from '@angular/common/http';
 import { Constants } from '../constatnts';
+import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +24,21 @@ export class PatientService {
     }
   );
 
+  }
+  minAge(minAge: number, errorMessage: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const birthDate = new Date(control.value);
+      const currentDate = new Date();
+  
+      const diffYears = currentDate.getFullYear() - birthDate.getFullYear();
+      const diffMonths = currentDate.getMonth() - birthDate.getMonth();
+      const diffDays = currentDate.getDate() - birthDate.getDate();
+  
+      if (diffYears < minAge || (diffYears === minAge && diffMonths < 0) || (diffYears === minAge && diffMonths === 0 && diffDays < 0)) {
+        return { 'minAge': { message: errorMessage } };
+      }
+  
+      return null;
+    };
   }
 }
