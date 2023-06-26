e28119647bf3c75897bf47703bc1201fcc8f8203
Abdelrahman-Amr <abdozyed2199@gmail.com>
2023-06-26 03:40:16 +0300

    fix change password

--- src/app/client/components/update-profile/update-user-data/update-user-data.component.ts
+++ src/app/client/components/update-profile/update-user-data/update-user-data.component.ts
@@ -14,39 +14,41 @@ import { UpdateProfileService } from 'src/app/shared/services/update-profile.ser
 })
 export class UpdateUserDataComponent implements OnInit {
   profileForm: FormGroup;
+  errorMsg='';
+
   //@ts-ignore
   patient:PatientModel=JSON.parse(localStorage.getItem('user'));
 
   constructor(private updateProfileService:UpdateProfileService
     ,private formBuilder: FormBuilder,private patientService:PatientService,private router: Router,public swaAlert:SwAlertService){
     this.updateProfileService.isActive='profile';
-    
-   
+
+
   }
 
 
   ngOnInit(): void {
-    
+
     this.profileForm = this.formBuilder.group({
       //@ts-ignore
-      name: [this.patient.fullName,[ Validators.minLength(3),
+      name: [this.patient.fullName,[Validators.required, Validators.minLength(3),
         Validators.maxLength(30)]],
       //@ts-ignore
-      email: [this.patient.email, [Validators.pattern(Constants.EMAIL)]],
+      email: [this.patient.email, [Validators.required,Validators.pattern(Constants.EMAIL)]],
       //@ts-ignore
-      phone: [this.patient.phoneNumber,[Validators.pattern(Constants.DIGITS_ONLY_11)]],
+      phone: [this.patient.phoneNumber,[Validators.required,Validators.pattern(Constants.DIGITS_ONLY_11)]],
       //@ts-ignore
-      birthday: [this.patient.birthDate,[this.patientService.minAge(15, 'You must be at least 15 years old')]],
+      birthday: [this.patient.birthDate,[Validators.required,this.patientService.minAge(15, 'You must be at least 15 years old')]],
     });
 
   }
-  
+
   submitForm(){
     if(this.profileForm.get('name')?.value!=null||this.profileForm.get('name')?.value!=''){
       this.patient.fullName=this.profileForm.get('name')?.value;
     }
     if(this.profileForm.get('email')?.value!=null||this.profileForm.get('email')?.value!=''){
-      this.patient.email=this.profileForm.get('email')?.value; 
+      this.patient.email=this.profileForm.get('email')?.value;
      }
 
      if(this.profileForm.get('phone')?.value!=null||this.profileForm.get('phone')?.value!=''){
@@ -56,7 +58,7 @@ export class UpdateUserDataComponent implements OnInit {
      if(this.profileForm.get('birthday')?.value!=null||this.profileForm.get('birthday')?.value!=''){
       this.patient.birthDate=this.profileForm.get('birthday')?.value;
      }
-    
+
 
     this.patientService.updatePatientProfile(this.patient);
     localStorage.setItem('user', JSON.stringify(this.patient));
